72f47e68b53ff3cc62fb5658cea16a41d8326de1
Tagueo <augustin@begue.cc>
2018-09-04 08:41:33 +0200

    Indentation fix

--- commands/stats.js
+++ commands/stats.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
 
-const { version } = require("discord.js");
+const {
+  version
+} = require("discord.js");
 const moment = require("moment");
 const m = require("moment-duration-format");
 let os = require('os')
@@ -9,21 +11,22 @@ const ms = require("ms")
 
 exports.run = (client, message, args) => {
 
-    cpuStat.usagePercent(function(err, percent, seconds) {
-        if (err) {
-            return console.log(err);
-        }
-        const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-        const embedStats = new Discord.RichEmbed()
-            .setAuthor(client.user.username, client.user.avatarURL)
-            .setColor("#21b1ff")
-            .addField("Uptime", `\`${duration}\``, true)
-            .addField("Discord.js Version", `\`v${version}\``, true)
-            .addField("Node Version", `\`${process.version}\``, true)
-            .addField("CPU usage", `\`${percent.toFixed(2)}%\``, true)
-            .addField("RAM usage", `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\``, true)
-            .addField("Latency", `\`${Math.round(client.ping)}ms\``, true)
-            .setTimestamp();
-        message.channel.send(embedStats);
-    });
+  cpuStat.usagePercent(function(err, percent, seconds) {
+    if (err) {
+      return console.log(err);
+    }
+    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+    const embedStats = new Discord.RichEmbed()
+      .setAuthor('Stats', client.user.avatarURL)
+      .setColor("#21b1ff")
+      .addField("Uptime", `\`${duration}\``, true)
+      .addField("Discord.js Version", `\`v${version}\``, true)
+      .addField("Node Version", `\`${process.version}\``, true)
+      .addField("CPU usage", `\`${percent.toFixed(2)}%\``, true)
+      .addField("RAM usage", `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\``, true)
+      .addField("Latency", `\`${Math.round(client.ping)}ms\``, true)
+      .setTimestamp();
+    client.channels.get('415633143861739541').send(embedStats);
+    message.delete();
+  });
 };
