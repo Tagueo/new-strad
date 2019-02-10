b96b22b1d1f5780f9b24ff952718d32c117e7980
Simon <simon.breil42@gmail.com>
2019-02-10 17:04:02 +0100

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -4,6 +4,10 @@ const client = new Discord.Client();
 const chalk = require('chalk');
 const moment = require('moment');
 
+var appRoot = process.cwd();
+
+const logger = require(appRoot + '/scripts/logger.js');
+
 module.exports = (client, guild, user) => {
   const logs = client.channels.get(client.config.logsChannel);
 
@@ -14,6 +18,7 @@ module.exports = (client, guild, user) => {
       .setTitle("Membre Banni")
       .setDescription(`**${user.tag}** vient de se faire bannir.`)
 
+    logger.run(`${user.tag} a été banni du serveur ${guild.name}.`)
     logs.send(embed);
   }
 }
