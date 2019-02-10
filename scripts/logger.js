4d927d8d4c4aee7fa2a0174657f225c25cb291f0
Simon <simon.breil42@gmail.com>
2019-02-10 11:48:04 +0100

    Ajout de logger.js
    Permet d'écrire des logs sur l'activité des serveurs sur lequel Strad se trouve.

---
+++ scripts/logger.js
@@ -0,0 +1,36 @@
+/**
+ * Permet d'écrire dans les logs.
+ */
+
+const moment = require('moment');
+
+exports.run = (message) => {
+
+    var fs = require("fs"); // Module nécessaire pour écrire dans les logs.
+    let isMessageType = typeof(message) != "string" ? true : false; // Détecte un message (objet) ou une string en argument.
+    var logMoment = moment(Date().now()).format("L - h:mm:ss");
+    var filter = /[\s]{2,}/gi;
+
+    var logMessage;
+
+    if (isMessageType) { // Traite la requête en fonction de son type.
+        logMessage = "[" + logMoment + "][" + message.guild.name + "|" + message.guild.id + "] ";
+        logMessage += message.author.tag + " : " + message.cleanContent.replace(filter, " ") + "\r\n";
+    } else {
+        logMessage = "[" + logMoment + "] * " + message + "\r\n";
+    }
+
+    try { // Essaie d'écrire dans les logs.
+        fs.appendFile(`./storage/logs/${logMoment}.txt`, logMessage, (err) => {})
+        var logFile = fs.readFileSync(`./storage/logs/${logMoment}.txt`);
+
+        console.log(logMessage);
+
+        logFile += logMessage;
+
+        fs.writeFile(`./storage/logs/${logMoment}.txt`, logFile);
+    } catch (error) {
+        
+    }
+
+}
