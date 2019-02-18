8f11a0f4610d6872f248e705bb7362d623f9586f
Simon <simon.breil42@gmail.com>
2019-02-18 11:40:20 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,20 +1,31 @@
 const Discord = require("discord.js")
+const config = require("../config.json");
 
 var mysql = require("mysql")
 
 exports.run = (client, message, args) => {
 
+  const query = `SELECT * FROM users WHERE user_id = ${message.author.id}`;
+  var results;
+
   var con = mysql.createConnection({
-    
-  })
+    host: "localhost",
+    user: config.mysqlUser,
+    password: config.mysqlPass,
+    database: "strad"
+  });
+
+  con.connect(query, (err) => {
+    results = result;
+  });
 
   const stradEmoji = "<:strad:544057514589683723>";
 
   const embedMoney = new Discord.RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
     .setThumbnail(message.author.avatarURL)
-    .addField("Valeur du compte", `{unknown} <:strad:544057514589683723>`, true)
-    .addField("Nombre de Créas", `{unknown}`, true)
+    .addField("Valeur du compte", `${results["money"]} <:strad:544057514589683723>`, true)
+    .addField("Nombre de Créas", `${results["creas_amount"]}`, true)
     .addField("ID du propriétaire", message.author.id, true)
     .addField("Rang artistique", "unknown", true)
     .setFooter("Strad rank")
