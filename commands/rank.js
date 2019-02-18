e378adaa2153a8b818a14cf0f06fc65072a1d565
Simon <simon.breil42@gmail.com>
2019-02-18 14:32:13 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -40,19 +40,19 @@ exports.run = (client, message, args) => {
 
   console.log("CHECKPOINT 2");
   
-  const stradEmoji = "<:strad:544057514589683723>";
+  // const stradEmoji = "<:strad:544057514589683723>";
 
-  const embedMoney = new Discord.RichEmbed()
-    .setAuthor(message.author.tag, message.author.avatarURL)
-    .setThumbnail(message.author.avatarURL)
-    .addField("Valeur du compte", `${gb.results.money} ${stradEmoji}`, true)
-    .addField("Nombre de Créas", `${gb.results.money}`, true)
-    .addField("ID du propriétaire", message.author.id, true)
-    .addField("Rang artistique", "unknown", true)
-    .setFooter("Strad rank")
-    .setColor(message.member.displayColor);
+  // const embedMoney = new Discord.RichEmbed()
+  //   .setAuthor(message.author.tag, message.author.avatarURL)
+  //   .setThumbnail(message.author.avatarURL)
+  //   .addField("Valeur du compte", `${gb.results.money} ${stradEmoji}`, true)
+  //   .addField("Nombre de Créas", `${gb.results.money}`, true)
+  //   .addField("ID du propriétaire", message.author.id, true)
+  //   .addField("Rang artistique", "unknown", true)
+  //   .setFooter("Strad rank")
+  //   .setColor(message.member.displayColor);
 
-    client.channels.get('413678978990080010').send(embedMoney);
-    message.delete();
+  //   client.channels.get('413678978990080010').send(embedMoney);
+  //   message.delete();
 
 };
