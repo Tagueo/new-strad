1970c5aec15e88da90259d0b1569163b7a7f5412
Simon <simon.breil42@gmail.com>
2019-02-18 11:33:26 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -4,17 +4,21 @@ var mysql = require("mysql")
 
 exports.run = (client, message, args) => {
 
+  var con = mysql.createConnection({
+    
+  })
+
   const stradEmoji = "<:strad:544057514589683723>";
 
   const embedMoney = new Discord.RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
     .setThumbnail(message.author.avatarURL)
-    .addField("ID du propriétaire", message.author.id, true)
     .addField("Valeur du compte", `{unknown} <:strad:544057514589683723>`, true)
     .addField("Nombre de Créas", `{unknown}`, true)
+    .addField("ID du propriétaire", message.author.id, true)
     .addField("Rang artistique", "unknown", true)
     .setFooter("Strad rank")
-    .setColor("#21b1ff");
+    .setColor(message.member.displayColor);
 
     client.channels.get('413678978990080010').send(embedMoney);
     message.delete();
