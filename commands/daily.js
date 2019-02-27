4c5a9db10818f16a33ac495fdf43cb4a79187ae1
Simon <simon.breil42@gmail.com>
2019-02-27 16:23:57 +0100

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -27,18 +27,21 @@ exports.run = (client, message, args, userData) => {
 
       if (rows[0].lastdaily != moment().format("DD/MM/AA")) {
 
-        gb.embed = new Discord.RichEmbed()
-          .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
-          .setColor("#6cb254")
-          .addField("**50** <:block:547449530610745364> ont été ajoutés sur ton compte !\nPour y accéder, fais : Strad rank")
-          .setFooter("Strad daily");
-
         con.query(`UPDATE users SET money = money + 50 WHERE user_id = "${message.author.id}"`, function (err, rows, fields) {
           if (err) {
             console.log(err);
           }
           con.end();
         })
+
+        gb.embed = new Discord.RichEmbed()
+          .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
+          .setColor("#6cb254")
+          .addField("**50** <:block:547449530610745364> ont été ajoutés sur ton compte !\nPour y accéder, fais : Strad rank")
+          .setFooter("Strad daily");
+
+        client.channels.get('413678978990080010').send(gb.embed);
+        message.delete();
 
       } else {
 
@@ -47,15 +50,15 @@ exports.run = (client, message, args, userData) => {
           .setColor("#ff6766")
           .addField("Tu as déjà obtenu ta récompense aujourd'hui.\nAttends demain avant de la récupérer !")
           .setFooter("Strad daily");
+
+        client.channels.get('413678978990080010').send(gb.embed);
+        message.delete();
         
         con.end();
 
       }
 
     });
-
-    client.channels.get('413678978990080010').send(gb.embed);
-    message.delete();
 
   } catch (err) {
     console.log(err);
