a68c308d0b2705a989789f945190488d9d6c0c16
Simon <simon.breil42@gmail.com>
2019-02-27 18:03:31 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
     .setAuthor('Help', client.user.avatarURL)
     .setColor("#21b1ff")
     .setDescription(`Pour m'appeler, utilise le prefix **Strad** (c'est mon nom !).\nExemple : \`Strad help\` pour afficher cet encart ^^`)
-    .addField(`Commandes`, `**help •** Permet d'afficher cet encart.\n**stats •** Permet d'afficher des statistiques me concernant.\n**repo •** Permet d'afficher le lien vers mon dépôt GitHub.`)
+    .addField(`Commandes`, `**help •** Affiche cet encart.\n**stats •** Affiche des informations me concernant.\n**repo •** Affiche le lien vers mon dépôt GitHub.\n**rank •** Affiche ton profil\n**daily •** Permet de récupérer ta récompense journalière !`)
 
     client.channels.get('415633143861739541').send(embed);
     message.delete();
