4860a7ec1aa879798a155495ad50d32953437ddb
Simon <simon.breil42@gmail.com>
2019-04-17 11:25:08 +0200

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, userData) => {
 
   try {
     // DB connection
-    var gb = {embed: undefined};
+    var gb = {embed: undefined, uv: undefined, dv: undefined};
     var mysql = require("mysql");
 
     var con = mysql.createConnection({
@@ -27,9 +27,39 @@ exports.run = (client, message, args, userData) => {
         console.log(err);
       }
 
-      if (rows[0].lastdaily != moment().format("DD/MM/YY")) {
+      if (rows[0].lastdaily != moment().format("DD/MM/YY")) { // Si l'utilisateur n'a pas encore demandé son daily aujourd'hui, alors...
 
-        con.query(`UPDATE users SET money = money + 50 WHERE user_id = "${message.author.id}"`, function (err, rows, fields) {
+        var finalReward = 50, finalCreaReward = 0;
+
+        con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "UV"`, function (err, rows, fields) { // Récupération des UVs
+          if (err) {
+            console.log(err);
+          }
+
+          gb.uv = rows.length;
+
+        })
+
+        con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "DV"`, function (err, rows, fields) { // Récupération des DVs
+          if (err) {
+            console.log(err);
+          }
+
+          gb.dv = rows.length;
+
+        })
+
+        finalReward += gb.uv * 5;
+        finalCreaReward += gb.uv - gb.dv;
+        if (finalCreaReward < 0) finalCreaReward = 0;
+
+        con.query(`DELETE FROM rewards WHERE rewarded_id = "${message.author.id}"`, function (err, rows, fields) { // Suppression des votes
+          if (err) {
+            console.log(err);
+          }
+        })
+
+        con.query(`UPDATE users SET money = money + ${finalReward}, creas_amount = creas_amount + ${finalCreaReward} WHERE user_id = "${message.author.id}"`, function (err, rows, fields) {
           if (err) {
             console.log(err);
           }
@@ -42,7 +72,9 @@ exports.run = (client, message, args, userData) => {
             gb.embed = new Discord.RichEmbed()
               .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
               .setColor("#6cb254")
-              .setDescription("**50** <:block:547449530610745364> ont été ajoutés sur ton compte !\nPour y accéder, fais : Strad rank")
+              .addField(`Blocs`, `+ **${finalReward}** <:block:547449530610745364>`, true)
+              .addField(`Créas`, `+ **${finalCreaReward}** <:crea:547482886824001539>`, true)
+              .setDescription(`Voici ta récompense journalière (${gb.uv} <:like:419568361110896640> / ${gb.dv} <:dislike:419568377946832896>) ! Pour accéder à ton compte, fais : Strad rank`)
               .setFooter("Strad daily");
 
             client.channels.get('415633143861739541').send(gb.embed);
