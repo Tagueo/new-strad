5a4161f3291aabfb0272dab9f9bac936df995a5d
Simon <simon.breil42@gmail.com>
2019-04-17 15:28:09 +0200

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
   // DB connection
 
   var gb = {
-    results: undefined
+    results: undefined, rank: undefined
   };
   var mysql = require("mysql");
   
@@ -30,21 +30,36 @@ exports.run = (client, message, args) => {
       
       gb.results = rows[0];
 
-      const stradEmoji = "<:block:547449530610745364>";
-      const creaEmoji = "<:crea:547482886824001539>";
-    
-      const embedMoney = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
-        .setThumbnail(message.author.avatarURL)
-        .addField("Valeur du compte", `${gb.results.money} ${stradEmoji}`, true)
-        .addField("Nombre de Créas", `${gb.results.creas_amount} ${creaEmoji}`, true)
-        .addField("Rang", `#0`, true)
-        .addField("Titre artistique", `${gb.results.rank}`, true)
-        .setFooter("Strad rank")
-        .setColor(message.member.displayColor);
-    
-        client.channels.get('415633143861739541').send(embedMoney);
-        message.delete();
+      con.query(`SELECT * FROM users ORDER BY creas_amount DESC`, function(err, rows, fields) {
+
+        if (err) {
+            console.log(err);
+        }
+
+        for (i=0;i<rows.length;i++) {
+          if (message.author.id == rows[i].user_id) {
+            gb.rank = i + 1;
+            break;
+          }
+        }
+
+        const stradEmoji = "<:block:547449530610745364>";
+        const creaEmoji = "<:crea:547482886824001539>";
+      
+        const embedMoney = new Discord.RichEmbed()
+          .setAuthor(message.author.tag, message.author.avatarURL)
+          .setThumbnail(message.author.avatarURL)
+          .addField("Valeur du compte", `${gb.results.money} ${stradEmoji}`, true)
+          .addField("Nombre de Créas", `${gb.results.creas_amount} ${creaEmoji}`, true)
+          .addField("Rang", `#${gb.rank}`, true)
+          .addField("Titre artistique", `${gb.results.rank}`, true)
+          .setFooter("Strad rank")
+          .setColor(message.member.displayColor);
+      
+          client.channels.get('415633143861739541').send(embedMoney);
+          message.delete();
+
+      });
 
     });
 
