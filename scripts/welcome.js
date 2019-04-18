a0bc29d0d75b4bdfd52771813e06daf9addbea23
Simon <simon.breil42@gmail.com>
2019-04-19 00:52:21 +0200

    Update welcome.js

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,5 +1,9 @@
 exports.run = function(member) {
 
-    member.send(`Bienvenue sur Stradivarius, ${member.displayName} ! On n'attendait plus que toi :wink:\nPrends le temps de lire les #règles puis présente-toi !`);
+    let welcomeMessage = `Bienvenue, toi :wink: Tu penses qu'on pourra devenir amis ?\n`
+        + `Au fait, je viens de t'ajouter le rôle d'**Apprenti**, le temps que tu te présentes dans le salon #présentation :smile:\n`
+        + `On a tous envie de te connaître ! :violin:\n\n`
+        + `> https://discord.gg/4MmJwgj`;
+    member.send();
 
 };
