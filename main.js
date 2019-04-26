6bc04497e95cac8589a5b99f85b0ee408868e3c4
Simon <simon.breil42@gmail.com>
2019-04-26 17:48:41 +0200

    Revert "Update main.js"
    This reverts commit 2b1bc96394f379ead5b29c71f18804196f4d13db.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const Enmap = require("enmap");
 const fs = require("fs");
 const chalk = require('chalk');
-const mysql = require('mysql');
 var moment = require('moment');
 
 const client = new Discord.Client();
