8a834a0fccc2f18f5c984a5a663da64a9fd1951d
Simon <simon.breil42@gmail.com>
2019-04-30 15:37:24 +0200

    Update gsay.js

--- commands/gsay.js
+++ commands/gsay.js
@@ -1,30 +1,28 @@
-const Discord = require("discord.js")
-
 exports.run = (client, message, args) => {
 
   if (message.member.roles.find(x => x.name === "Mentor")) {
 
     if (!args[0]) {
-    var channel = message.guild.channels.get(message.channel.id);
-} else {
-    var channel = message.guild.channels.get(args[0]) || message.guild.channels.find("name", args[0]);
-    if (!channel) {
-        var tempChannel = args[0];
-        tempChannel = tempChannel.slice(2, -1);
-        var channel = message.guild.channels.get(tempChannel);
+        var channel = message.guild.channels.get(message.channel.id);
+    } else {
+        var channel = message.guild.channels.get(args[0]) || message.guild.channels.find("name", args[0]);
+        if (!channel) {
+            var tempChannel = args[0];
+            tempChannel = tempChannel.slice(2, -1);
+            var channel = message.guild.channels.get(tempChannel);
+        }
+        if (!channel) {
+            error.run(client, message, 'Channel invalide\nUsage : gsay <channel> <message>');
+        }
     }
-    if (!channel) {
-        error.run(client, message, 'Channel invalide\nUsage : .ignorechannel on/off <channel>');
-    }
-}
 
     if (channel && args[1]) {
-      args.splice(0, 1);
-      client.channels.get(channel).send(args.join(' '));
-      message.author.send('<:true:413685423202893826> Envoyé !');
+        args.splice(0, 1);
+        client.channels.get(channel).send(args.join(' '));
+        message.author.send('<:true:413685423202893826> Envoyé !');
     }
 
   } else {
-    message.channel.send('Tu n\'as pas le droit de faire cette commande <:facepalm:428261651947716609>');
+      message.channel.send('Tu n\'as pas le droit de faire cette commande <:facepalm:428261651947716609>');
   }
 };
