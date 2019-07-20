65147d6ac8e52d68a31c196807315cf9964e1c34
Simon <simon.breil42@gmail.com>
2019-07-20 14:31:51 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,21 +2,16 @@ const chalk = require('chalk');
 const Discord = require('discord.js');
 var moment = require('moment');
 
-const fetcher = require('../scripts/fetcher');
-
 module.exports = async (client) => {
     var stradivarius = client.guilds.find(g => g.id === "412369732679893004");
 
     console.log(`[${chalk.cyan(moment(Date.now()).format('h:mm:ss'))}] [${chalk.yellow(client.user.tag)}] Pret à servir dans ${chalk.cyan(client.channels.size)} channels sur ${chalk.cyan(client.guilds.size)} serveurs, pour un total de ${chalk.cyan(client.users.size)} utilisateurs.`);
     client.user.setActivity(`Strad help`, {
-        type: 'WATCHING'
+        type: 'STREAMING',
+        url: 'https://www.twitch.tv/delphistudio'
     })
         .then(presence => console.log(`[${chalk.cyan(moment(Date.now()).format('h:mm:ss'))}] [${chalk.yellow(client.user.tag)}] Activity set`))
         .catch(console.error);
-
-    // // Mise en cache des 50 derniers messages de chaque salon créatif...
-    // await fetcher.run(stradivarius);
-    // console.log("Messages mis en cache !");
 
     // Distributeur de rôles
     var roleMsgId = "570618282177069076", notifMsgId = "601739344163897344";
