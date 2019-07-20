267a1c53a0e5edf80a74abde503d9a5346b2bb7a
Simon <simon.breil42@gmail.com>
2019-07-20 18:53:30 +0200

    Interface de la boutique

--- commands/shop.js
+++ commands/shop.js
@@ -2,13 +2,22 @@ const Discord = require("discord.js");
 const db = require("../scripts/db");
 const mp = require("../scripts/msgPresets");
 
-function formatItem(itemId) {
-    // TODO À compléter
+function addItem(embed, item) {
+    var notSaleableText;
+    if (item.saleable === 1)
+        notSaleableText = "";
+    else
+        notSaleableText = "\nCet item ne peut être vendu.";
+    if (item.emoji.length > 1) item.emoji = client.emojis.get(item.emoji);
+    embed
+        .addField(`${item.id}. ${item.buy_amount} x ${item.name} ${item.emoji}`, `${item.description}\n\n"
+            + "Prix : ${item.price} <:block:547449530610745364>` + notSaleableText);
 }
 
 exports.run = (client, message, args) => {
 
     if (!message.member.roles.find(r => r.name === "Mentor")) {
+        message.delete();
         mp.sendWIP(client.channels.get('415633143861739541'));
         return;
     } // TODO À retirer après le développement
@@ -16,10 +25,32 @@ exports.run = (client, message, args) => {
     var con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
 
     sql = "SELECT item_id AS id, item_name AS name, item_emoji AS emoji, description, price, is_buyable AS buyable,"
-        + " is_saleable AS saleable, quantity, buy_amount, discount, script_name, type FROM items WHERE is_buyable = 1";
+        + " is_saleable AS saleable, quantity, buy_amount, discount, script_name, type FROM items WHERE is_buyable = 1 ORDER BY id ASC";
     con.query(sql, {}, (rows) => {
         var shopEmbed = new Discord.RichEmbed()
             .setAuthor("Boutique")
+            .addField("Aide", "Pour acheter un article dans la boutique, tape la commande **Strad buy"
+                + " <numéro de l'article>**.\nExemple : Strad buy 1 (pour acheter un changement de pseudonyme)\n"
+                + "Pour accéder aux détails d'un article, tape la commande **Strad view <numéro de l'article>**.");
+        rows["id"].forEach(id => {
+            let item = {
+                id: id,
+                name: rows["name"][id - 1],
+                emoji: rows["emoji"][id - 1],
+                description: rows["description"][id - 1],
+                price: rows["price"][id - 1],
+                buyable: rows["buyable"][id - 1],
+                saleable: rows["saleable"][id - 1],
+                quantity: rows["quantity"][id - 1],
+                buy_amount: rows["buy_amount"][id - 1],
+                discount: rows["discount"][id - 1],
+                script_name: rows["script_name"][id - 1],
+                type: rows["type"][id - 1]
+            };
+            if (item.buyable === 1) addItem(shopEmbed, item);
+        });
+        message.channel.send(shopEmbed); // TODO Mettre l'id du salon #commandes après le développement
+
         con.end();
     });
 
