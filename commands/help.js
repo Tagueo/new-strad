ccc60d9629db0d6bfdd8fc94c32a0dc2b23ebd98
Simon <simon.breil42@gmail.com>
2019-07-21 17:07:52 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,9 +7,11 @@ exports.run = (client, message) => {
         .setColor(mLog.colors.NEUTRAL_BLUE)
         .setDescription("Pour m'appeler, utilise le prefix **Strad** (c'est mon nom !)."
             + "\nExemple : ``Strad help`` pour afficher cet encart ^^")
-        .addField("Commandes", "``help`` • Affiche cet encart.\n``stats`` • Affiche des informations "
-            + "me concernant.\n``repo`` • Affiche le lien vers mon dépôt GitHub.\n``rank`` • Affiche ton profil."
-            + "\n``top`` • Affiche le classement du serveur.\n``daily`` • Permet de récupérer ta récompense journalière !");
+        .addField("Général", "``help`` • Affiche cet encart."
+            + "\n``rank`` • Affiche ton profil."
+            + "\n``top`` • Affiche le classement du serveur.\n``daily`` • Permet de récupérer ta récompense journalière !")
+        .addField("Divers", "``repo`` • Affiche le lien vers mon dépôt GitHub.\n``stats`` • Affiche des informations me concernant.")
+        .setFooter("Strad help");
 
     client.channels.get('415633143861739541').send(embed);
     message.delete();
