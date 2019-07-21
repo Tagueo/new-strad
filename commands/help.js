33ef3e7ebaf11775953425b62728d860eff8e7df
Simon <simon.breil42@gmail.com>
2019-07-21 17:14:53 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ exports.run = (client, message) => {
         .addField("Général", "``help`` • Affiche cet encart."
             + "\n``rank`` • Affiche ton profil."
             + "\n``top`` • Affiche le classement du serveur.\n``daily`` • Permet de récupérer ta récompense journalière !")
-        .addField("Boutique", "[BIENTÔT !] ``shop`` • Ouvre la boutique du serveur.\n[BIENTÔT] ``buy <numéro de l'article>`` "
+        .addField("Boutique", "[BIENTÔT !] ``shop`` • Ouvre la boutique du serveur.\n[BIENTÔT !] ``buy <numéro de l'article>`` "
             + "• Permet d'acheter l'article de boutique dont le numéro est spécifié en argument.")
         .addField("Divers", "``repo`` • Affiche le lien vers mon dépôt GitHub.\n``stats`` • Affiche des informations me concernant.")
         .setFooter("Strad help");
