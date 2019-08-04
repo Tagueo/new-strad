1eebdb6bca7d7c8110ba3f8322c2908f8dd45a9c
Simon <simon.breil42@gmail.com>
2019-08-04 18:34:31 +0200

    Update sendMP.js

--- scripts/sendMP.js
+++ scripts/sendMP.js
@@ -1,11 +1,10 @@
 exports.run = (client, messageContent, member) => {
-    try {
-        member.send(messageContent);
-    } catch (e) {
-        let errorEmbed = new Discord.RichEmbed()
-            .setAuthor("Envoi de message privé impossible")
-            .setDescription(`Merci de signaler à ${member.username} que nous ne pouvons pas poursuivre la procédure en cours qu'il a lancée car il m'a bloqué !`)
-            .setColor(mLog.colors.ALERT);
-        client.channels.get('415633143861739541').send(errorEmbed);
-    }
+    member.send(messageContent)
+        .catch((e) => {
+            let errorEmbed = new Discord.RichEmbed()
+                .setAuthor("Envoi de message privé impossible")
+                .setDescription(`Merci de signaler à ${member.username} que nous ne pouvons pas poursuivre la procédure en cours qu'il a lancée car il m'a bloqué !`)
+                .setColor(mLog.colors.ALERT);
+            client.channels.get('415633143861739541').send(errorEmbed);
+        });
 };
