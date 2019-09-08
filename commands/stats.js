9ad32ca484c96bf27217419b462f39b229436aca
Simon <simon.breil42@gmail.com>
2019-09-08 17:18:00 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -19,7 +19,7 @@ exports.run = async (client, message, args) => {
       .addField("Version de Discord.js", `\`v${version}\``, true)
       .addField("Version de NodeJS", `\`${process.version}\``, true)
       .addField("Utilisation du CPU", `\`${percent.toFixed(2)}%\``, true)
-      .addField("Utilisation de la RAM", `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\``, true)
+      .addField("Utilisation de la RAM", `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\``, true)
       .addField("Latence", `\`${Math.round(client.ping)} ms\``, true)
       .setTimestamp();
     client.channels.get('415633143861739541').send(embedStats);
