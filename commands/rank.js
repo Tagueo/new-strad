f11a90af6b7c0d2ec248925e438069e3ff171c11
Simon <simon.breil42@gmail.com>
2019-09-08 16:37:06 +0200

    Rank

--- commands/rank.js
+++ commands/rank.js
@@ -3,64 +3,60 @@ const db = require("../scripts/db.js");
 
 exports.run = async (client, message, args) => {
 
-    try {
+    let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
+    let rows = await con.query(`SELECT *
+                                FROM users
+    WHERE user_id = ${message.author.id}`);
 
-        let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
-
-        con.query(`SELECT * FROM users WHERE user_id = ${message.author.id}`, {}, rows => {
-
-            if (!rows) {
-                con.end();
-                return;
-            }
-
-            con.query(`SELECT * FROM users ORDER BY creas_amount DESC`, {"results": rows[0], "rank": undefined}, (rows, dg) => {
-                for (i=0;i<rows.length;i++) {
-                    if (message.author.id == rows[i].user_id) {
-                        dg["rank"] = i + 1;
-                        break;
-                    }
-                }
-
-                con.query(`SELECT has_items.item_id AS id, item_emoji AS emoji, has_items.amount AS amount FROM has_items INNER JOIN items ON items.item_id = has_items.item_id`
-                    + ` WHERE user_id = "${message.author.id}" AND amount != 0 ORDER BY has_items.item_id ASC`, {}, rows => {
-                    const stradEmoji = "<:block:547449530610745364>";
-                    const creaEmoji = "<:crea:547482886824001539>";
-                    let inventory = [];
-
-                    if (rows[0]) {
-                        for (let i=0;i<rows.length;i++) {
-                            let emoji = client.emojis.get(rows[i]["emoji"]);
-                            inventory[i] = `${emoji} x ${rows[i]["amount"]}`;
-                        }
-                    }
-
-                    inventory = inventory.join(" • ");
-
-                    if (inventory === "") inventory = "Ton inventaire est vide. Fais ``Strad shop`` pour acheter des items !";
-
-                    const embedMoney = new Discord.RichEmbed()
-                        .setAuthor(message.author.tag, message.author.avatarURL)
-                        .setThumbnail(message.author.avatarURL)
-                        .addField("Valeur du compte", `${dg["results"].money} ${stradEmoji}`, true)
-                        .addField("Nombre de Créas", `${dg["results"].creas_amount} ${creaEmoji}`, true)
-                        .addField("Rang", `#${dg["rank"]}`, true)
-                        .addField("Titre artistique", dg["results"].rank, true)
-                        .addField("Inventaire", inventory)
-                        .setFooter("Strad rank")
-                        .setColor(message.member.displayColor);
-
-                    client.channels.get('415633143861739541').send(embedMoney);
-                    message.delete();
-
-                    con.end();
-                })
-            })
-
-        });
-
-    } catch (e) {
-        console.log(e);
+    if (!rows) {
+        con.end();
+        return;
     }
+
+    let users = await con.query(`SELECT *
+                                 FROM users
+                                 WHERE creas_amount <> 0
+                                 ORDER BY creas_amount DESC`),
+        userItems = await con.query(`SELECT has_items.item_id AS id, item_emoji AS emoji, has_items.amount AS amount
+                                    FROM has_items INNER JOIN items ON items.item_id = has_items.item_id
+                                    WHERE user_id = "${message.author.id}" AND amount != 0
+                                    ORDER BY has_items.item_id ASC`),
+        inventory = [],
+        rank = 0,
+        user = rows[0];
+    const stradEmoji = "<:block:547449530610745364>";
+    const creaEmoji = "<:crea:547482886824001539>";
+
+    for (i = 0; i < rows.length; i++) {
+        if (message.author.id == users[i].user_id) {
+            rank = i + 1;
+            break;
+        }
+    }
+
+    if (userItems[0]) {
+        for (let i = 0; i < rows.length; i++) {
+            let emoji = client.emojis.get(rows[i].emoji);
+            inventory[i] = `${emoji} x ${rows[i].amount}`;
+        }
+    }
+
+    inventory = inventory ? inventory.join(" • ") : "Ton inventaire est vide. Fais ``Strad shop`` pour acheter des items !";
+
+    const rankEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setThumbnail(message.author.avatarURL)
+        .addField("Valeur du compte", `${user.money} ${stradEmoji}`, true)
+        .addField("Nombre de Créas", `${user.creas_amount} ${creaEmoji}`, true)
+        .addField("Rang", `#${rank}`, true)
+        .addField("Titre artistique", user.rank, true)
+        .addField("Inventaire", inventory)
+        .setFooter("Strad rank")
+        .setColor(message.member.displayColor);
+
+    client.channels.get('415633143861739541').send(rankEmbed);
+    message.delete();
+
+    con.end();
 
 };
