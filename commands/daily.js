5e22d100c01d609061dcbfdfe8071368769acfc1
Simon <simon.breil42@gmail.com>
2019-09-08 15:59:23 +0200

    Key

--- commands/daily.js
+++ commands/daily.js
@@ -6,60 +6,54 @@ const db = require("../scripts/db");
 exports.run = async (client, message, args) => {
 
     moment.locale("fr");
+    
+    let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
+    let user = (await con.query(`SELECT * FROM users WHERE user_id = "${message.author.id}"`))[0];
 
-    try {
+    if (user.lastdaily !== moment().format("DD/MM/YY")) { // Si l'utilisateur n'a pas encore demandé son daily aujourd'hui, alors...
+        let upvotes = await con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "UV"`),
+            downvotes = await con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "DV"`),
+            finalBlockReward = 50,
+            finalCreaReward = 0;
 
-        let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
-        let user = (await con.query(`SELECT * FROM users WHERE user_id = "${message.author.id}"`))[0];
+        upvotes = upvotes ? upvotes.length : 0;
+        downvotes = downvotes ? downvotes.length : 0;
 
-        if (user.lastdaily !== moment().format("DD/MM/YY")) { // Si l'utilisateur n'a pas encore demandé son daily aujourd'hui, alors...
-            let upvotes = await con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "UV"`),
-                downvotes = await con.query(`SELECT * FROM rewards WHERE rewarded_id = "${message.author.id}" AND type = "DV"`),
-                finalBlockReward = 50,
-                finalCreaReward = 0;
+        finalBlockReward += upvotes * 5;
+        finalCreaReward += upvotes - downvotes;
 
-            upvotes = upvotes ? upvotes.length : 0;
-            downvotes = downvotes ? downvotes.length : 0;
+        if (finalCreaReward < 0) finalCreaReward = 0;
 
-            finalBlockReward += upvotes * 5;
-            finalCreaReward += upvotes - downvotes;
-
-            if (finalCreaReward < 0) finalCreaReward = 0;
-
-            await con.query(`DELETE FROM rewards WHERE rewarded_id = "${message.author.id}"`); // Suppression des votes
-            await con.query(`UPDATE users SET money = money + ${finalBlockReward}, creas_amount = creas_amount
+        await con.query(`DELETE FROM rewards WHERE rewarded_id = "${message.author.id}"`); // Suppression des votes
+        await con.query(`UPDATE users SET money = money + ${finalBlockReward}, creas_amount = creas_amount
                 + ${finalCreaReward}, usertag = "${message.member.displayName}"
                 WHERE user_id = "${message.author.id}"`); // Ajout des Blocs et des Créas
-            await con.query(`UPDATE users SET lastdaily = "${moment().format('DD/MM/YY')}"
+        await con.query(`UPDATE users SET lastdaily = "${moment().format('DD/MM/YY')}"
                 WHERE user_id = "${message.author.id}"`); // Mise à jour de la date du dernier daily
 
-            let successEmbed = new Discord.RichEmbed()
-                .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
-                .setColor(mLog.colors.VALID)
-                .addField(`Blocs`, `+ **${finalBlockReward}** <:block:547449530610745364>`, true)
-                .addField(`Créas`, `+ **${finalCreaReward}** <:crea:547482886824001539>`, true)
-                .setDescription(`Voici ta récompense journalière ! Pour accéder à ton compte, fais \`\`Strad rank\`\`.\n**${upvotes} <:like:568493894270976012> / ${downvotes} <:dislike:568493872968368149>**`)
-                .setFooter("Strad daily");
+        let successEmbed = new Discord.RichEmbed()
+            .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
+            .setColor(mLog.colors.VALID)
+            .addField(`Blocs`, `+ **${finalBlockReward}** <:block:547449530610745364>`, true)
+            .addField(`Créas`, `+ **${finalCreaReward}** <:crea:547482886824001539>`, true)
+            .setDescription(`Voici ta récompense journalière ! Pour accéder à ton compte, fais \`\`Strad rank\`\`.\n**${upvotes} <:like:568493894270976012> / ${downvotes} <:dislike:568493872968368149>**`)
+            .setFooter("Strad daily");
 
-            client.channels.get('415633143861739541').send(successEmbed);
-            message.delete();
+        client.channels.get('415633143861739541').send(successEmbed);
+        message.delete();
 
-            con.end();
-        } else {
-            let errorEmbed = new Discord.RichEmbed()
-                .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
-                .setColor(mLog.colors.ALERT)
-                .setDescription(`Tu as déjà obtenu ta récompense aujourd'hui.\nRécupère-la ${moment().endOf("day").fromNow()} !`)
-                .setFooter("Strad daily");
+        con.end();
+    } else {
+        let errorEmbed = new Discord.RichEmbed()
+            .setAuthor("Récompense quotidienne (" + message.member.displayName + ")", message.author.avatarURL)
+            .setColor(mLog.colors.ALERT)
+            .setDescription(`Tu as déjà obtenu ta récompense aujourd'hui.\nRécupère-la ${moment().endOf("day").fromNow()} !`)
+            .setFooter("Strad daily");
 
-            client.channels.get('415633143861739541').send(errorEmbed);
-            message.delete();
+        client.channels.get('415633143861739541').send(errorEmbed);
+        message.delete();
 
-            con.end();
-        }
-
-    } catch (err) {
-        console.log(err);
+        con.end();
     }
 
 };
--- commands/key.js
+++ commands/key.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const db = require("../scripts/db");
 const mLog = require("../scripts/mLog");
 const sendMP = require("../scripts/sendMP");
-var moment = require("moment");
+let moment = require("moment");
 
 exports.run = async (client, message, args) => {
 
@@ -83,63 +83,56 @@ exports.run = async (client, message, args) => {
     let keyFace = createKey(), keyPrint = createFingerPrint(), todayDate = moment().format('DD/MM/YY');
     let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
 
-    con.query(`SELECT money FROM users WHERE user_id = "${message.author.id}"`, {}, rows => {
+    let money = (await con.query(`SELECT money FROM users WHERE user_id = "${message.author.id}"`))[0].money,
+        keys = await con.query(`SELECT * FROM blocks_keys`);
 
-        let money = rows[0]["money"];
+    if (chosenValue > money) {
+        let errorEmbed = new Discord.RichEmbed()
+            .setAuthor("Création de clé impossible")
+            .setDescription(`Tu n'as pas assez de Blocs pour créer cette clé. Il te manque **${chosenValue - money}** <:block:547449530610745364> !`)
+            .setColor(mLog.colors.ALERT);
+        message.delete();
+        commandChannel.send(errorEmbed);
+        con.end();
+        return;
+    }
 
-        if (chosenValue > money) {
-            let errorEmbed = new Discord.RichEmbed()
-                .setAuthor("Création de clé impossible")
-                .setDescription(`Tu n'as pas assez de Blocs pour créer cette clé. Il te manque **${chosenValue - money}** <:block:547449530610745364> !`)
-                .setColor(mLog.colors.ALERT);
-            message.delete();
-            commandChannel.send(errorEmbed);
-            con.end();
-            return;
+    if (keys[0]) {
+        while (keyExists(keys, keyFace)) {
+            keyFace = createKey();
         }
+        while (printExists(keys, keyPrint)) {
+            keyPrint = createFingerPrint();
+        }
+    }
 
-        con.query(`SELECT * FROM blocks_keys`, {}, keys => {
+    await con.query(`UPDATE users SET money = money - ${chosenValue} WHERE user_id = "${message.author.id}"`);
+    await con.query(`INSERT INTO blocks_keys (key_face, key_print, key_value, creator_id, creation_date)
+        VALUES ("${keyFace}", "${keyPrint}", ${chosenValue}, "${message.author.id}",
+                "${moment().format('DD/MM/YY')}")`);
 
-            if (keys[0]) {
-                while (keyExists(keys, keyFace)) {
-                    keyFace = createKey();
-                }
-                while (printExists(keys, keyPrint)) {
-                    keyPrint = createFingerPrint();
-                }
-            }
+    let publicSuccessEmbed = new Discord.RichEmbed()
+        .setAuthor("Création effectuée")
+        .setDescription("Ta clé a correctement été débloquée. Tu viens de la recevoir en message privé !")
+        .setFooter("Strad key <valeur>")
+        .setColor(mLog.colors.VALID);
+    message.delete();
+    commandChannel.send(publicSuccessEmbed);
 
-            con.query(`INSERT INTO blocks_keys (key_face, key_print, key_value, creator_id, creation_date) VALUES ("${keyFace}", "${keyPrint}", ${chosenValue},
-                "${message.author.id}", "${moment().format('DD/MM/YY')}")`, {}, rows => {
-                con.query(`UPDATE users SET money = money - ${chosenValue} WHERE user_id = "${message.author.id}"`, {}, rows => {
+    let privateSuccessEmbed = new Discord.RichEmbed()
+        .setAuthor("Création de clé")
+        .setDescription("Voici ta clé d'une valeur de **" + chosenValue + "** <:block:547449530610745364> (clique pour l'afficher) :\n"
+            + "||```" + keyFace + "```||\nFais bien attention de ne pas la partager à n'importe qui !\n"
+            + "Afin de l'utiliser, le bénéficiaire de la clé devra taper la commande : ``Strad redeem <clé>``. Il recevra ainsi la valeur en Blocs de la clé !")
+        .addField("Empreinte de la clé", "```" + keyPrint + "```"
+            + "\nNote : L'empreinte n'est pas secrète, elle est directement liée à ta clé. Tu peux partager l'empreinte au destinataire de celle-ci "
+            + "afin d'attester qu'elle t'appartient, qu'elle est valide et qu'elle a bien la valeur en Blocs annoncée. Cela peut se révéler bien utile dans le cas d'un échange !")
+        .setColor(mLog.colors.NEUTRAL_BLUE);
+    sendMP.run(client, privateSuccessEmbed, message.member);
 
-                    let publicSuccessEmbed = new Discord.RichEmbed()
-                        .setAuthor("Création effectuée")
-                        .setDescription("Ta clé a correctement été débloquée. Tu viens de la recevoir en message privé !")
-                        .setFooter("Strad key <valeur>")
-                        .setColor(mLog.colors.VALID);
-                    message.delete();
-                    commandChannel.send(publicSuccessEmbed);
+    mLog.run(client, "Création de clé", message.author + " a créé une clé d'empreinte ``" + keyPrint + "`` et d'une valeur de **" + chosenValue + "** <:block:547449530610745364>.",
+        mLog.colors.NEUTRAL_BLUE);
 
-                    let privateSuccessEmbed = new Discord.RichEmbed()
-                        .setAuthor("Création de clé")
-                        .setDescription("Voici ta clé d'une valeur de **" + chosenValue + "** <:block:547449530610745364> (clique pour l'afficher) :\n"
-                            + "||```" + keyFace + "```||\nFais bien attention de ne pas la partager à n'importe qui !\n"
-                            + "Afin de l'utiliser, le bénéficiaire de la clé devra taper la commande : ``Strad redeem <clé>``. Il recevra ainsi la valeur en Blocs de la clé !")
-                        .addField("Empreinte de la clé", "```" + keyPrint + "```"
-                            + "\nNote : L'empreinte n'est pas secrète, elle est directement liée à ta clé. Tu peux partager l'empreinte au destinataire de celle-ci "
-                            + "afin d'attester qu'elle t'appartient, qu'elle est valide et qu'elle a bien la valeur en Blocs annoncée. Cela peut se révéler bien utile dans le cas d'un échange !")
-                        .setColor(mLog.colors.NEUTRAL_BLUE);
-                    sendMP.run(client, privateSuccessEmbed, message.member);
-
-                    mLog.run(client, "Création de clé", message.author + " a créé une clé d'empreinte ``" + keyPrint + "`` et d'une valeur de **" + chosenValue + "** <:block:547449530610745364>.",
-                        mLog.colors.NEUTRAL_BLUE);
-
-                    con.end();
-
-                });
-            });
-        });
-    });
+    con.end();
 
 };
