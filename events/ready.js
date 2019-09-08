271f90fffd7480d64b11416676202d72ab6dfdd4
Simon <simon.breil42@gmail.com>
2019-09-08 18:29:28 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,8 @@
 const chalk = require('chalk');
-const Discord = require('discord.js');
-var moment = require('moment');
+let moment = require('moment');
 
 module.exports = async (client) => {
-    var stradivarius = client.guilds.find(g => g.id === "412369732679893004");
+    let stradivarius = client.guilds.find(g => g.id === "412369732679893004");
 
     console.log(`[${chalk.cyan(moment(Date.now()).format('h:mm:ss'))}] [${chalk.yellow(client.user.tag)}] Pret à servir dans ${chalk.cyan(client.channels.size)} channels sur ${chalk.cyan(client.guilds.size)} serveurs, pour un total de ${chalk.cyan(client.users.size)} utilisateurs.`);
     client.user.setActivity(`Strad help`, {
@@ -14,7 +13,7 @@ module.exports = async (client) => {
         .catch(console.error);
 
     // Distributeur de rôles
-    var roleMsgId = "570618282177069076", notifMsgId = "601739344163897344";
+    let roleMsgId = "570618282177069076", notifMsgId = "601739344163897344";
     stradivarius.channels.find(c => c.id === "570605835172970496").fetchMessage(roleMsgId);
     stradivarius.channels.find(c => c.id === "570605835172970496").fetchMessage(notifMsgId);
 };
