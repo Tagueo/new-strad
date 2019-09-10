9a378732940cafa434ee5e163e0c4bbe7ab12bf1
Simon <simon.breil42@gmail.com>
2019-09-10 14:19:08 +0200

    DB

--- commands/rank.js
+++ commands/rank.js
@@ -3,63 +3,61 @@ const db = require("../scripts/db.js");
 
 exports.run = async (client, message, args) => {
 
-    let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad")
-        .then(() => {
-            let rows = await con.query(`SELECT *
+    let con = new db.Connection("localhost", client.config.mysqlUser, client.config.mysqlPass, "strad");
+    let rows = await con.query(`SELECT *
                                 FROM users
                                 WHERE user_id = ${message.author.id}`);
 
-            if (!rows) {
-                con.end();
-                return;
-            }
+    if (!rows) {
+        con.end();
+        return;
+    }
 
-            let users = await con.query(`SELECT *
+    let users = await con.query(`SELECT *
                                  FROM users
                                  WHERE creas_amount <> 0
                                  ORDER BY creas_amount DESC`),
-                userItems = await con.query(`SELECT has_items.item_id AS id, item_emoji AS emoji, has_items.amount AS amount
+        userItems = await con.query(`SELECT has_items.item_id AS id, item_emoji AS emoji, has_items.amount AS amount
                                     FROM has_items INNER JOIN items ON items.item_id = has_items.item_id
                                     WHERE user_id = "${message.author.id}" AND amount != 0
                                     ORDER BY has_items.item_id ASC`),
-                inventory = [],
-                rank = 0,
-                user = rows[0];
-            const stradEmoji = "<:block:547449530610745364>";
-            const creaEmoji = "<:crea:547482886824001539>";
+        inventory = [],
+        rank = 0,
+        user = rows[0];
+    const stradEmoji = "<:block:547449530610745364>";
+    const creaEmoji = "<:crea:547482886824001539>";
 
-            for (i = 0; i < rows.length; i++) {
-                if (message.author.id == users[i].user_id) {
-                    rank = i + 1;
-                    break;
-                }
-            }
+    for (i = 0; i < rows.length; i++) {
+        if (message.author.id == users[i].user_id) {
+            rank = i + 1;
+            break;
+        }
+    }
 
-            if (userItems[0]) {
-                for (let i = 0; i < userItems.length; i++) {
-                    let emoji = client.emojis.get(userItems[i].emoji);
-                    inventory[i] = `${emoji} x ${userItems[i].amount}`;
-                }
-            }
+    if (userItems[0]) {
+        for (let i = 0; i < userItems.length; i++) {
+            let emoji = client.emojis.get(userItems[i].emoji);
+            inventory[i] = `${emoji} x ${userItems[i].amount}`;
+        }
+    }
 
-            inventory = inventory.join(" • ");
-            if (inventory === "") inventory = "Ton inventaire est vide. Fais ``Strad shop`` pour acheter des items !";
+    inventory = inventory.join(" • ");
+    if (inventory === "") inventory = "Ton inventaire est vide. Fais ``Strad shop`` pour acheter des items !";
 
-            const rankEmbed = new Discord.RichEmbed()
-                .setAuthor(message.author.tag, message.author.avatarURL)
-                .setThumbnail(message.author.avatarURL)
-                .addField("Valeur du compte", `${user.money} ${stradEmoji}`, true)
-                .addField("Nombre de Créas", `${user.creas_amount} ${creaEmoji}`, true)
-                .addField("Rang", `#${rank}`, true)
-                .addField("Titre artistique", user.rank, true)
-                .addField("Inventaire", inventory)
-                .setFooter("Strad rank")
-                .setColor(message.member.displayColor);
+    const rankEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setThumbnail(message.author.avatarURL)
+        .addField("Valeur du compte", `${user.money} ${stradEmoji}`, true)
+        .addField("Nombre de Créas", `${user.creas_amount} ${creaEmoji}`, true)
+        .addField("Rang", `#${rank}`, true)
+        .addField("Titre artistique", user.rank, true)
+        .addField("Inventaire", inventory)
+        .setFooter("Strad rank")
+        .setColor(message.member.displayColor);
 
-            client.channels.get('415633143861739541').send(rankEmbed);
-            message.delete();
+    client.channels.get('415633143861739541').send(rankEmbed);
+    message.delete();
 
-            con.end();
-        });
+    con.end();
 
 };
--- scripts/db.js
+++ scripts/db.js
@@ -3,19 +3,17 @@ let mysql = require("mysql");
 exports.Connection = class Connection {
 
     constructor(host, user, pass, dbname) {
-        return await new Promise((resolve, reject) => {
-            this.core = mysql.createConnection({
-                host: host,
-                user: user,
-                password: pass,
-                database: dbname,
-                charset: "utf8_general_ci"
-            });
-            this.core.connect((err) => {
-                if (err) throw err;
-            });
-            resolve(true);
+        this.core = mysql.createConnection({
+            host: host,
+            user: user,
+            password: pass,
+            database: dbname,
+            charset: "utf8_general_ci"
         });
+        this.core.connect((err) => {
+            if (err) throw err;
+        });
+        resolve(true);
     }
 
     async query(sql) {
